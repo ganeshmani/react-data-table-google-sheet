2518bd62fb745b78d47a197e85b85d46ce6dc6e1
mac <ganeshmani009@gmail.com>
2020-08-07 23:54:38 +0530

    changes

--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -1,37 +1,6 @@
 import React from "react";
-import { useTable, useGlobalFilter } from "react-table";
+import { useTable } from "react-table";
 
-// Create an editable cell renderer
-const EditableCell = ({
-  value: initialValue,
-  row: { index },
-  column: { id },
-  updateMyData, // This is a custom function that we supplied to our table instance
-}) => {
-  // We need to keep and update the state of the cell normally
-  const [value, setValue] = React.useState(initialValue);
-
-  const onChange = (e) => {
-    setValue(e.target.value);
-  };
-
-  // We'll only update the external data when the input is blurred
-  const onBlur = () => {
-    updateMyData(index, id, value);
-  };
-
-  // If the initialValue is changed external, sync it up with our state
-  React.useEffect(() => {
-    setValue(initialValue);
-  }, [initialValue]);
-
-  return <input value={value} onChange={onChange} onBlur={onBlur} />;
-};
-
-// Set our editable cell renderer as the default Cell renderer
-const defaultColumn = {
-  Cell: EditableCell,
-};
 const Table = ({ columns, data, updateMyData, skipPageReset }) => {
   const {
     getTableProps,
@@ -43,7 +12,6 @@ const Table = ({ columns, data, updateMyData, skipPageReset }) => {
   } = useTable({
     columns,
     data,
-    // defaultColumn,
     updateMyData,
     autoResetPage: !skipPageReset,
   });
